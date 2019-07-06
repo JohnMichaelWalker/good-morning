74e356c76f1389b4b2bc8ee114a1dd976b5ea8f4
unknown <johnmichaelwalker2@gmail.com>
2019-07-06 22:25:05 +0100

    Updates

---
+++ precache-manifest.34d7fdd837ca059fc20310fb6b3a05a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4ee45e6d6bf9f9522e8",
+    "url": "/good-morning/static/js/runtime~main.a4ee45e6.js"
+  },
+  {
+    "revision": "4b10b22ec2a12bc60479",
+    "url": "/good-morning/static/js/main.4b10b22e.chunk.js"
+  },
+  {
+    "revision": "eaa00beb736e7116163d",
+    "url": "/good-morning/static/js/1.eaa00beb.chunk.js"
+  },
+  {
+    "revision": "4b10b22ec2a12bc60479",
+    "url": "/good-morning/static/css/main.0cff34af.chunk.css"
+  },
+  {
+    "revision": "eaa00beb736e7116163d",
+    "url": "/good-morning/static/css/1.fe7e6b1e.chunk.css"
+  },
+  {
+    "revision": "83674080c5bbd699c02ae4ec710e0ec1",
+    "url": "/good-morning/index.html"
+  }
+];
